015546ae0a7e1908bd210bcd2d96b64af0009ae7
iam <iam@ivan-room.lan>
2021-09-23 23:50:54 +0300

    update

--- BotBack/src/main/java/ru/spb/iam/NonCommand.java
+++ BotBack/src/main/java/ru/spb/iam/NonCommand.java
@@ -1,13 +1,18 @@
 package ru.spb.iam;
 
 import com.github.prominence.openweathermap.api.OpenWeatherMapClient;
-// import com.github.prominence.openweathermap.api.enums.Language;
+import com.github.prominence.openweathermap.api.enums.Language;
 import com.github.prominence.openweathermap.api.enums.UnitSystem;
+import com.github.prominence.openweathermap.api.model.Coordinate;
 import com.github.prominence.openweathermap.api.model.weather.Weather;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.List;
 
 
 public class NonCommand {
@@ -18,29 +23,42 @@ public class NonCommand {
         logger.debug(String.valueOf(update));
         Long chatId = msg.getChatId();
         String userName = Utils.getUserName(msg);
+        String answer = "Не знаю что ответить на это. Обратитесь к Алисе.. \uD83D\uDE25";
 
         logger.debug(String.format("Пользователь %s. Начата обработка сообщения \"%s\", не являющегося командой",
                 userName, String.valueOf(msg)));
 
-        if (msg.hasLocation()) return "Прислано местоположение";
+        if (msg.hasLocation()) {
 
-        String answer = "Это нонкоманд заглушка";
+            answer = String.valueOf(getWeatherByLL(msg.getLocation().getLatitude(), msg.getLocation().getLongitude()));
 
- //       OpenWeatherMapClient openWeatherClient = new OpenWeatherMapClient(System.getenv("OPEN_WEATHER_TOKEN"));
+            return answer;
+        } else {
 
-//        final Weather weather = openWeatherClient
-//                .currentWeather()
-//                .single()
-//                .byCityName("Minsk")
-//                .language(Language.RUSSIAN)
-//                .unitSystem(UnitSystem.METRIC)
-//                .retrieve()
-//                .asJava();
-//
-//        logger.debug(String.format("Погода в Минске: \"%s\"", String.valueOf(weather)));
-
-        logger.debug(String.format("Пользователь %s. Завершена обработка сообщения \"%s\", не являющегося командой",
-                userName, String.valueOf(msg)));
-        return answer;
+            logger.debug(String.format("Пользователь %s. Завершена обработка сообщения \"%s\", не являющегося командой",
+                    userName, String.valueOf(msg)));
+            return answer;
+        }
     }
+
+    private Weather getWeatherByLL(Double latitude, Double longitude) {
+
+        OpenWeatherMapClient openWeatherClient = new OpenWeatherMapClient(System.getenv("OPEN_WEATHER_TOKEN"));
+
+        final Weather weather = openWeatherClient
+                .currentWeather()
+                .single()
+                .byCoordinate(Coordinate.of(latitude, longitude))
+                .language(Language.RUSSIAN)
+                .unitSystem(UnitSystem.METRIC)
+                .retrieve()
+                .asJava();
+
+        logger.debug(String.format("Погода в по координатам: \"%s\"", String.valueOf(weather)));
+        logger.debug(String.format("Координаты: \"%s\"", String.valueOf(Coordinate.of(53.54, 27.34))));
+
+        return weather;
+
+    }
+
 }
